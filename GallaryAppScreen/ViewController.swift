d8d3c46837fdf3a4ac8a8902aad30a468fc45640
Ivan Sokol <sokolivan34@mail.ru>
2022-11-15 11:13:59 +0300

    Initial Commit

---
+++ GallaryAppScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GallaryAppScreen
+//
+//  Created by Ваня Сокол on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
